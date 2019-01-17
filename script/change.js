dd57972dadb491d3cebfd2fc9ec67f6adc1bfebb
simon fouin <simon.fouin@gmail.com>
2019-01-17 18:50:09 +0100

    f-more_monney: add new monney type to convert

--- script/change.js
+++ script/change.js
@@ -1,12 +1,23 @@
 $(function() {
+	
+	$('.form-group select').change(function() {
+		var value = $(this).val();
+		$(this).next("input").attr("data-value", value).keyup();
+	})
+
 	$('.form-group').keyup(function() {
 
 		var children = $(this).children("input");
 		var quotient = children.attr("data-value");
 		var value = children.val();
 		
-		var over_value = (value*quotient).toFixed(2)
-		
-		var over_element = $(this).prev().length > 0 ? $(this).prev().children("input").val(over_value) : $(this).next().children("input").val(over_value)
+		var next_children = $(this).prev().length > 0 ? $(this).prev().children("input") : $(this).next().children("input")
+		var next_quotient = next_children.attr("data-value");
+
+		var new_value = (value/quotient*next_quotient).toFixed(2)
+
+		next_children.val(new_value)	
 	})
+
+
 })
