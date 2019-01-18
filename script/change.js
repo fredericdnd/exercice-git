45643d02da271ba28a5305b379c9e1a6bf2d56e1
simon fouin <simon.fouin@gmail.com>
2019-01-18 14:47:40 +0100

    h-better js: var to let

--- script/change.js
+++ script/change.js
@@ -1,20 +1,20 @@
 $(function() {
 	
 	$('.form-group select').change(function() {
-		var value = $(this).val();
+		let value = $(this).val();
 		$(this).next("input").attr("data-value", value).keyup();
 	})
 
 	$('.form-group').keyup(function() {
 
-		var children = $(this).children("input");
-		var quotient = children.attr("data-value");
-		var value = children.val();
+		let children = $(this).children("input");
+		let quotient = children.attr("data-value");
+		let value = children.val();
 		
-		var next_children = $(this).prev().length > 0 ? $(this).prev().children("input") : $(this).next().children("input")
-		var next_quotient = next_children.attr("data-value");
+		let next_children = $(this).prev().length > 0 ? $(this).prev().children("input") : $(this).next().children("input")
+		let next_quotient = next_children.attr("data-value");
 
-		var new_value = (value/quotient*next_quotient).toFixed(2)
+		let new_value = (value/quotient*next_quotient).toFixed(2)
 
 		next_children.val(new_value)	
 	})
